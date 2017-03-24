5b09d28f27cff59a9e0ab9226aab264fca360f35
Tobias Olbrich <public.tobias.olbrich@gmail.com>
2017-03-24 22:14:56 +0100

    Added some input validation

--- OpenReports.py
+++ OpenReports.py
@@ -2,11 +2,17 @@
 
 import requests
 import json as js
+import rfc3987
 from subprocess import call
+
+prefix = 'https://stackoverflow.com/a/'
 
 remote = requests.get('http://samserver.bhargavrao.com:8000/napi/api/reports/all?filter=010000')
 data = js.loads(remote.text)
 links = [item['link'] for item in data['items']]
+
+if not all((rfc3987.match(link, rule='IRI') and link[:len(prefix)] == prefix) for link in links):
+    raise RuntimeError('Invalid link received')
 
 if len(links) == 1:
     call(['firefox', '-new-window'] + links)
