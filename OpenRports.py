fe3b0947b9df0df557d43fa114711bb46b1f2f9e
Tobias Olbrich <public.tobias.olbrich@gmail.com>
2017-03-24 17:25:06 +0100

    Working example, but vulnerable

---
+++ OpenRports.py
@@ -0,0 +1,17 @@
+from urllib.request import urlretrieve
+import json as js
+from subprocess import call
+
+remote = urlretrieve('http://samserver.bhargavrao.com:8000/napi/api/reports/all?filter=010000')
+tempFile = open(remote[0])
+data = js.load(tempFile)
+links = [item['link'] for item in data['items']]
+
+if len(links) == 1:
+    call(['firefox', '-new-window'] + links)
+elif len(links) > 1:
+    call(['firefox'] + links)
+else:
+    print('All reports have been tended too.')
+
+
